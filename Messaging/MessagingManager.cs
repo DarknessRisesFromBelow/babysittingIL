cd4d58a97d0778a8e2424170ae5aed2649058cde
YairMedina <99030228+DarknessRisesFromBelow@users.noreply.github.com>
2023-04-26 17:52:06 +0300

    added the ability to see messages sent by yourself

--- Messaging/MessagingManager.cs
+++ Messaging/MessagingManager.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using YMA.Management;
 using babysittingIL.UserManagement;
+using System.Linq;
+
 namespace babysittingIL.Messaging
 {
 	class MessagingManager
@@ -10,10 +12,23 @@ namespace babysittingIL.Messaging
 		public static string ReadAll(int index)
 		{
 			string messages = "";
-			for(int i  = Lists[index].Messages.Count - 1; i > 0; i--)
+			Console.WriteLine("index : " + index);
+			Parallel.For(0, Lists[index].Messages.Count, pos=>
 			{
-				messages += Lists[index].Messages[i].sender + " : " + Lists[index].Messages[i].message + " : " + user.GetUserByID(Lists[index].Messages[i].sender).GetUsername() + " ||";
-			}
+				int i = Lists[index].Messages.Count - 1 - pos;
+				messages += Lists[index].Messages[i].sender + " : " + Lists[index].Messages[i].message + " : " + user.GetUserByID(Lists[index].Messages[i].sender).GetUsername() + " : " + "0" +" ||";
+			});
+			Parallel.For(0, Lists.Count, listIndex =>
+			{
+				for(int i = 0; i < Lists[listIndex].Messages.Count; i++)
+				{
+					if(Lists[listIndex].Messages[i].sender == index)
+					{
+						messages += Lists[listIndex].Messages[i].reciver + " : " + Lists[listIndex].Messages[i].message + " : " + user.GetUserByID(Lists[listIndex].Messages[i].reciver).GetUsername() + " : " + "1" +" ||";		
+					}
+				}
+			});
+			Console.WriteLine("messages were : " + messages);
 			return messages;
 		}
 	}
